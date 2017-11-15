153731fa6a5ddd800c56caac211894e522d6e574
Carlos Barragan <starttheshooting@hotmail.com>
2017-11-15 17:12:55 -0600

    Render Content conditionally

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     persons: [
       { name: 'Pepe', age: 10 },
       { name: 'Pks', age: 11 }    
-    ]
+    ], 
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -33,6 +34,13 @@ class App extends Component {
     )
   }
 
+  tooglePersons = () => {
+    const doesShowPersons = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShowPersons
+    });
+  }
+
   render() {
 
     const customButton = {
@@ -47,16 +55,20 @@ class App extends Component {
       <div className="App">
         <h1>Hi I'm a React App</h1>
         {/* The inneficient way to bind data */}
-        <button style={customButton} onClick={() => this.switchNameHandler('MadMax!!!')}>Swap Name</button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}
-          // This is the best way tho bind data
-          click={this.switchNameHandler.bind(this , 'MADMAX')}>This is a children Text!</Person>
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          changeName={this.nameChangeHandler}/>
+        <button style={customButton} onClick={this.tooglePersons}>Show Persons</button>
+        { this.state.showPersons ? //this is how you show stuff like ng-if in angular
+          <div>
+            <Person 
+              name={this.state.persons[0].name} 
+              age={this.state.persons[0].age}
+              // This is the best way tho bind data
+              click={this.switchNameHandler.bind(this , 'MADMAX')}>This is a children Text!</Person>
+            <Person 
+              name={this.state.persons[1].name} 
+              age={this.state.persons[1].age}
+              changeName={this.nameChangeHandler}/>
+          </div>: null
+        }
       </div>
     );
   }
