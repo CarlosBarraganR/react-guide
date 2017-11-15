be86049f66373f42c6c4f274b45574011eb37eff
Carlos Barragan <starttheshooting@hotmail.com>
2017-11-15 15:29:06 -0600

    State, and states manipulation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,32 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Pepe', age: 10 },
+      { name: 'Pks', age: 11 }    
+    ]
+  }
+
+  switchNameHandler = () => {
+    // DON'T DO THIS: this.state.persons[0].name = 'NewName'; 
+    this.setState(
+      {
+        persons: [
+          { name: 'Mac', age: 22 },
+          { name: 'SusyQ', age: 44 }  
+        ]
+      }
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
-        <Person name="Carlos" age="25">This is a children Text!</Person>
+        <button onClick={this.switchNameHandler}>Swap Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>This is a children Text!</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>This is a children Text!</Person>
       </div>
     );
   }
