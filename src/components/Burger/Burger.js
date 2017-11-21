d1599f187921ccd667bdbee198c3b6cd0631060b
Carlos Barragan <starttheshooting@hotmail.com>
2017-11-21 16:39:35 -0600

    Burger automatic render via state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,23 @@ import './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    //In this case our props we receive are an object not an array so we have to cast to an array like this:
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map( ingredientsKey => {
+            return [...Array(props.ingredients[ingredientsKey])].map((_, index) => {
+                return <BurgerIngredient key={ingredientsKey + index} type={ingredientsKey}/>
+            });
+        })
+        .reduce( (acum, current) => { return acum.concat(current) }, [] );
+
+        if ( transformedIngredients.length === 0){
+            transformedIngredients = <p>Please add some Ingredients</p>
+        }
+
     return (
         <div className="Burger">
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="salad"/>
-            <BurgerIngredient type="bacon"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    //CAN STILL BE USED BUT ES6 IMPLEMENTS IT BY DEFAULT AND YOU ONLY NEED TO DECLARE THE STATE
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
+        }
+    }
+
     render(){
         return (
             <div>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Burger Controls Comp</div>
             </div>
         );
