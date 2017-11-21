f6d300385c5055ccbbb275775d9143fa096305e2
Carlos Barragan <starttheshooting@hotmail.com>
2017-11-21 09:52:51 -0600

    New Project stucture

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Layout.css';
+
+const layout = ( props ) => (
+    <div>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className="Content">
+           {props.children}
+        </main>
+    </div>
+);
+
+export default layout;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class BurgerBuilder extends Component {
+    render(){
+        return (
+            <div>
+                <div>Burger Component</div>
+                <div>Burger Controls Comp</div>
+            </div>
+        );
+    }
+}
+
+export default BurgerBuilder;
