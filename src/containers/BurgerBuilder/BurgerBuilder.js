e2c7c1e633447c8eddef3bab43ca887a297b137d
Carlos Barragan <starttheshooting@hotmail.com>
2017-11-21 12:46:14 -0600

    BurgerBuilder And BurgerIngredient with propType validation

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
     render(){
         return (
             <div>
-                <div>Burger Component</div>
+                <Burger/>
                 <div>Burger Controls Comp</div>
             </div>
         );
