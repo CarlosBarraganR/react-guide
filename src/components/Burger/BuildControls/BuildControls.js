ede047a0948a1e991135fc1181adcba71819c3d0
Carlos Barragan <starttheshooting@hotmail.com>
2017-11-23 10:35:20 -0600

    Builder Controls Logic Working

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,10 +12,16 @@ const controls = [
     
 ];
 
-const buildControls = (props) => (
+const buildControls = ( props ) => (
     <div className="BuildControls">
-        {controls.map(control => (
-            <BuildControl label={control.label} type={control.type}/>
+        {controls.map( control => (
+            <BuildControl
+             key = { control.label } 
+             label = { control.label } 
+             type = { control.type } 
+             added = { () => props.ingredientAdded( control.type ) }
+             removed = { () => props.ingredientRemoved( control.type ) }
+             disabled = { props.ingredientsDisabled[control.type] }/>
         ))}
     </div>
 );
