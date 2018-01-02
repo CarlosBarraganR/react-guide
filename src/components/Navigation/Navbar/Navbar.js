70f4550829233e64b9086ee1dbb4e3a23502e2b6
Carlos Barragan <starttheshooting@hotmail.com>
2018-01-02 11:38:32 -0600

    Fixed NavItem folder position

--- src/components/Navigation/Navbar/Navbar.js
+++ src/components/Navigation/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Navbar.css';
-import NavItems from '../NavItems/NavItems';
+import NavItems from '../Navbar/NavItems/NavItems';
 import logo from '../../../assets/burger-logo.png';
 
 const Navbar = (props) => (
