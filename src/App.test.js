ce0f8983aca620da0f9b9408674ae277b4758600
Carlos Barragan <starttheshooting@hotmail.com>
2018-01-03 10:55:24 -0600

    New project structure

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,5 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  expect(divs.length).toBeGreaterThan(0);
+  ReactDOM.render(<App />, div);
 });
